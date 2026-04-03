52fe0c505106b6777407aeff3d73cf7fc51cc8af
MarlonKuna26 <marlonguevarap12@gmail.com>
2026-04-03 14:48:53 -0500

    rutas

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './src/routes/authRoutes.js';
 import userRoutes from './src/routes/userRoutes.js';
 import notificationRoutes from './src/routes/notificationRoutes.js';
+import cartRoutes from './src/routes/cartRoutes.js'; 
 import { notFound, errorHandler } from './src/middlewares/errorMiddleware.js';
 
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/notifications', notificationRoutes);
+app.use('/api/cart', cartRoutes);
 
 // Health check
 app.get('/api/health', (req, res) => {
